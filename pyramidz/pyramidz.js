d54fc999e77b19f5603b8cb3a0f880f09c624224
jkwok91 <0111291@gmail.com>
2013-12-08 15:11:58 -0500

    took out some redundant information
    may have reduced clarity
    but nothing will be more clear than reimplementng this with prototype. so. lemme just get this down to bare necessities right now
    
    this is getting a little unreadable, unfortunately. add comments.

--- pyramidz/pyramidz.js
+++ pyramidz/pyramidz.js
@@ -1,12 +1,9 @@
 //pyramids!!!
 //ok we won't do this with objects. i still know how.
 
-/*
-eventually i will move this to another file but for now i just wanna jot down something really quick
-game setup
-*/
 var pDeck = [];
 var flipDeck = [];
+var pyramid = [];
 
 /*
 helper functions to be used in initializing the game
@@ -23,8 +20,21 @@ function createDeck() {
   return cards;
 }
 
+function getValue(value) {
+  if (value == 0) {
+    return 'K';
+  } else if (value == 1) {
+    return 'A';
+  } else if (value == 11) {
+    return 'J';
+  } else if (value == 12) {
+    return 'Q';
+  } else {
+    return value.toString();
+  }
+}
+
 function cardElement(identifier) {
-  //the card object
   var playingCard = $("<div></div>");
   var card;
   if (typeof(identifier) == 'number') {
@@ -35,21 +45,9 @@ function cardElement(identifier) {
     card = identifier;
   }
   var suit = card['suit'];
-  playingCard.attr('data-suit',suit);
   var value = card['value'];
   playingCard.attr('data-value',value);
-  if (value == 0) {
-    value = 'K';
-  } else if (value == 1) {
-    value = 'A';
-  } else if (value == 11) {
-    value = 'J';
-  } else if (value == 12) {
-    value = 'Q';
-  } else {
-    value = value.toString();
-  }
-  playingCard.text(value+'o'+suit);
+  playingCard.text(getValue(value)+'o'+suit);
   return playingCard;
 }
 
@@ -63,75 +61,33 @@ function shuffle(deck) {
 
 function createPyramid() {
   // take in args a deck that will be used in creating a pyramid
-  var pyramid = [];
   var pyramidDiv = $("<div id='pyramid'></div>"); 
   // line up the rows of the pyramid
   var n = 0;
   for (var row = 0; row < 7; row++) {
-    pyramid[row] = [];
     n += row //the index of the 0th position of each row
     for (var pos = 0; pos <= row; pos++) {
       var cardHolder = {};
       var LI; 
-      var idx = n+pos;
-      cardHolder['card'] = idx;
-      var left = (n+(row+1))+pos;
+      var left = (n+pos)+(row+1);
       var right = left + 1
       cardHolder['left'] = (pDeck[left] != undefined) ? left : undefined;
       cardHolder['right'] = (pDeck[right] != undefined) ? right : undefined;
-      var hidden = !((cardHolder['left'] == undefined) && (cardHolder['right'] == undefined));
-      cardHolder['hidden'] = hidden;
-      if (hidden) {
+      if (!((cardHolder['left'] == undefined) && (cardHolder['right'] == undefined))) {
         LI = $("<div></div>");
         LI.text("XoY");
         LI.addClass("hidden");
       } else {
-        LI = cardElement(idx);
+        LI = cardElement(n+pos);
         LI.addClass("unhidden");
       }
-      pyramid[row][pos] = cardHolder;
+      pyramid.push(cardHolder);
       pyramidDiv.append(LI);
     }
     pyramidDiv.append($("<br/>"));
   }
   $("#game").append(pyramidDiv);
-  /*
-  pyramidObj['print'] = function () {
-    //will take in an argument that is an array of pyramid rows
-    var pyramidList = $("<div id='pyramid'></div>");
-    for (var row = 0; row < 7; row++) {
-      for (var pos = 0; pos <= row; pos++) {
-        var idx = pyramid[row][pos]['card'];
-        var LI = $("<div></div>"); //pyramidList.find('li:nth-of-type('+(idx+1)+')');
-        var hidden = pyramid[row][pos]['hidden']; //need to put in this property
-        //if status is hidden, display XoX instead, and also don't make it clickable
-        if (hidden) {
-          LI.text("XoY");
-          LI.addClass("hidden");
-        } else {
-          var card = pDeck[idx];
-          var suit = card['suit'];
-          var value = card['value'];
-          if (value == 0) {
-            value = 'K';
-          } else if (value == 1) {
-            value = 'A';
-          } else if (value == 11) {
-            value = 'J';
-          } else if (value == 12) {
-            value = 'Q';
-          } else {
-            value = value.toString();
-          }
-          LI.text(value+"o"+suit);
-        }
-        pyramidList.append(LI);
-      }
-      pyramidList.append($("<br/>"));
-    }
-    $("#game").append(pyramidList);
-  };
-  */
+  $("#game").append($("<div id='stack'></div>"));
   return pyramid;
 }
 
@@ -143,11 +99,11 @@ initialize game
 - and printing pyramid to the browser window
 */
 function createPyramidsGame() {
+  $("#game").append($("<div id='error'></div><br/>"));
   // create a deck
   var myDeck = createDeck();
-  var LENGTH_PDECK = 28;
   // randomly select 28 cards from it to form the pDeck
-  for (var i = 0; i < LENGTH_PDECK; i++) {
+  for (var i = 0; i < 28; i++) {
     var idx = Math.floor(Math.random()*myDeck.length);
     pDeck.push(myDeck.splice(idx,1)[0]);
   }
@@ -162,8 +118,8 @@ function createPyramidsGame() {
   var currentCard = flipDeck.pop();
   // connect the pyramid and the currentCard to divs
   var ccard = cardElement(currentCard); 
-  $("#game").append($("<br/><label for='current'>Current Card: </label>"));
-  $("#game").append(ccard);
+  $("#stack").append($("<br/><label for='current'>Current Card: </label>"));
+  $("#stack").append(ccard);
   var newCardButton = $("<button></button>");
   newCardButton.click(newCard);
   $("#game").append(newCardButton); 
@@ -184,7 +140,12 @@ function playGame() {
 function newCard() {
   $('#current').remove();
   var x = flipDeck.pop();
-  $("#game").append(cardElement(x));
+  if (typeof(x) == 'undefined') {
+    console.log("YOU HAVE LOST");
+    $("#stack").append($("<div>U LOSE-- NO MO CARDS</div>"));
+  } else {
+    $("#stack").append(cardElement(x));
+  }
 }
 
 function useCard() {
@@ -193,34 +154,42 @@ function useCard() {
   var value = $(this).data('value');
   var idx = $(this).data('idx');
   if (((value+1)%13 == current) || ((current+1)%13 == value)) {
-    console.log("current was",current);
+    $("#error").text('');
     currentCard.data('value',value);
-    currentCard.data('suit',$(this).data('suit'));
     currentCard.text($(this).text());
     //remove the card I just used
     //$(this).remove();
     $(this).html("&nbsp;&nbsp;&nbsp");
     /*
     $(this).removeData('value');
-    $(this).removeData('suit');
     $(this).removeData('idx')
     */
     //update status of nth card / remove from pile, allow cards under it to be updated
+    updateStatus(idx);
   } else {
-    console.log("invalid move because"+currentCard.text()+", having value"+current+" is not one more or one less than "+$(this).text()+", having value"+value);
+    $("#error").text("invalid move because "+$(this).text()+" is not one more or one less than "+currentCard.text());
   }
 }
 
-function updateStatus(card) {
-  // removeClass hidden
-  // maybe addClass unhidden -- haven't decided whether or not this class is necessary yet
-  // data-card-val = cardval
-  // data-card-suit = cardsuit
-  
-  // check if nth element in pyramid is hidden
-  // if nth element in pyramid is not hidden then update the li:nth-type-of(n)
-  // set the data-value attr to pyramid[n]['value']
-  // set the data-suit attr to pyramid[n]['suit']
-
-
+function updateStatus(cardidx) {
+  var checkThese = $('.hidden');
+  for (var i = 0; i < checkThese.length; i++) {
+    console.log("checking pyramid[",i);
+    pyramid[i]['left'] = (pyramid[i]['left'] == cardidx) ? undefined : pyramid[i]['left'];
+    pyramid[i]['right'] = (pyramid[i]['right'] == cardidx) ? undefined : pyramid[i]['right'];
+    if ((typeof(pyramid[i]['left']) == 'undefined') && (typeof(pyramid[i]['right']) == 'undefined')) {
+      var cardDiv = $('#pyramid > div:nth-of-type('+(i+1)+')');
+      console.log(cardDiv);
+      cardDiv.removeClass('hidden').addClass('unhidden');
+      var card = pDeck[i];
+      var value = card['value'];
+      var suit = card['suit'];
+      cardDiv.attr('data-value',value);
+      cardDiv.attr('data-idx',i);
+      cardDiv.text(getValue(value)+'o'+suit);
+      cardDiv.click(useCard);
+    } else {
+      //do nothing
+    }
+  }
 }
